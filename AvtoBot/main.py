dfc7d35c873045bf88bc810998085d8d1cc0f012
Dias Bauyrzhan <diasbauyrzhan@Dias-MacBook-Air.local>
2026-05-10 11:57:32 +0500

    Added Tax Calculations principle

--- AvtoBot/main.py
+++ AvtoBot/main.py
@@ -16,6 +16,25 @@ def is_valid_plate(text: str) -> bool:
     pattern = r"^\d{3}[A-Za-z]{3}$"
     return bool(re.match(pattern, text))
 
+def calculate_tax(engine_volume: float) -> int:
+    if engine_volume <= 0 or engine_volume > 10:
+        return -1
+
+    if engine_volume <= 1.1:
+        return 1 * MPR
+    elif engine_volume <= 1.5:
+        return 2 * MPR
+    elif engine_volume <= 2:
+        return 3 * MPR
+    elif engine_volume <= 2.5:
+        return 6 * MPR
+    elif engine_volume <= 3:
+        return 9 * MPR
+    elif engine_volume <= 4:
+        return 15 * MPR
+    else:
+        return 117 * MPR
+
 def main():
     TOKEN = "8718820157:AAFxzuX1KYZBmdetMT3fKdQF_8CO5atvNHM"
 
